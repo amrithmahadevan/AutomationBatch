c60c61e0da4e8a52a05107b322a9d4860af9cd66
Amrith Mahadevan <160821963+amrithmahadevan@users.noreply.github.com>
2024-05-30 18:22:20 +0530

    Choosing different browsers

---
+++ SeleniumTutorial/src/Basics/MultipleBrowsers.java
@@ -0,0 +1,33 @@
+package Basics;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class MultipleBrowsers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String browserName="Firefox";	//or any as req
+		WebDriver driver=null;
+		
+		if(browserName.equalsIgnoreCase("Chrome")){
+			driver = new ChromeDriver();
+		}
+		
+		else if(browserName.equalsIgnoreCase("Firefox")){
+			driver = new FirefoxDriver();
+		}
+		
+		else if(browserName.equalsIgnoreCase("msedge")) {
+			driver = new EdgeDriver();
+		}
+		
+		driver.manage().window().maximize();
+		driver.get("https://meet.google.com/");
+		
+	}
+
+}
