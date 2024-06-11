fa194644d1532f2f9fa384cc9c62a97306a5d903
Amrith Mahadevan <160821963+amrithmahadevan@users.noreply.github.com>
2024-06-11 13:56:46 +0530

    ****

---
+++ SeleniumTutorial/src/basics/DataTypeUnderstanding.java
@@ -0,0 +1,21 @@
+package basics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataTypeUnderstanding {
+	public static void main(String [] args) {
+		
+		 int a =3;
+		// String name= "mandy";
+		 
+		List<String> names = new ArrayList<String>();
+		
+		names.add("mandy");
+		names.add("cadny");
+		
+		for(String name : names) {
+			System.out.println(name);
+		}
+	}
+}
---
+++ SeleniumTutorial/src/uiComponents/Scrolling.java
@@ -0,0 +1,43 @@
+package uiComponents;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Scrolling {
+	static WebDriver driver;
+
+	public static void main(String[] args) {
+
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		//scrollByValue(300);
+		WebElement desiredElement = driver.findElement(By.cssSelector("[class='totalAmount']"));
+		scrollToElement(desiredElement);
+		scrollToBottomofPage() ;
+		
+		driver.quit();
+
+	}
+
+	
+	public static void scrollToElement(WebElement desiredElement) {
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("arguments[0].scrollIntoView(true);", desiredElement);
+	}
+
+	public static void scrollByValue(int value) {
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		// scroll up to some y cordinate
+		js.executeScript("window.scrollBy(0,300);");
+	}
+	
+	public static void scrollToBottomofPage() {
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scrollBy(0,document.body.scrollHeight);");
+	}
+	
+}
